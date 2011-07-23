0bf5d973f25f44f43809fe99da6f94f7fd00660c
Roy van de Water <me@royvandewater.com>
2011-07-23 12:41:39 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recepten::Application.config.secret_token = 'bddf3453211176ea4a1d0e5b61c3c1134e5f44d38d7039699c4aaefb1390d150d1183b078bb54eedc7e5fe626760a1a8ae9dfb66bfa1e2a039b430afaad2e641'
