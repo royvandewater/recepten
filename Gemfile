1dc5c3c0ee4207542303db03bbe1c07e73058a78
Roy van de Water <me@royvandewater.com>
2011-07-23 14:45:51 -0700

    Added jquery and blueprint

--- Gemfile
+++ Gemfile
@@ -4,21 +4,12 @@ gem 'rails', '3.0.9'
 gem 'rake', '0.8.7'
 gem 'mysql'
 
-# Deploy with Capistrano
-# gem 'capistrano'
-
-
 gem 'heroku'
+gem 'jquery-rails'
 
-# Bundle the extra gems:
-# gem 'bj'
-# gem 'nokogiri'
-# gem 'sqlite3-ruby', :require => 'sqlite3'
-# gem 'aws-s3', :require => 'aws/s3'
-
-# Bundle gems for the local environment. Make sure to
-# put test-only gems in this group so their generators
-# and rake tasks are available in development mode:
 group :development, :test do
   gem 'ruby-debug19', :require => 'ruby-debug'
+  gem 'rspec-rails'
+  gem 'factory_girl_rails'
+  gem 'capybara'
 end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,67 @@
+class RecipesController < ApplicationController
+
+  respond_to :html, :json
+
+  def index
+    @recipes = Recipe.all
+
+    respond_with @recipes
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+
+    respond_with @recipe
+  end
+
+  def new
+    @recipe = Recipe.new
+
+    respond_with @recipe
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def create
+    @recipe = Recipe.new(params[:recipe])
+
+      # if @recipe.save
+      #   respond_with @recipe, :status => :created, :location => @recipe, :notice => 'Recipe was successfully created.'
+      # else
+      # end
+
+      if @recipe.save
+        format.html { redirect_to(@recipe, :notice => 'Recipe was successfully created.') }
+        format.json  { render :json => @recipe, :status => :created, :location => @recipe }
+      else
+        format.html { render :action => "new" }
+        format.json  { render :json => @recipe.errors, :status => :unprocessable_entity }
+      end
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    respond_to do |format|
+      if @recipe.update_attributes(params[:recipe])
+        format.html { redirect_to(@recipe, :notice => 'Recipe was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @recipe.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(recipes_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
