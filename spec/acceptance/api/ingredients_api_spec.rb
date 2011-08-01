94d35afb923a46d427bf35727fcc5b0bbd299003
Roy van de Water <me@royvandewater.com>
2011-07-31 19:12:27 -0700

    Added ingredients_api_spec

---
+++ spec/acceptance/api/ingredients_api_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+feature 'Ingredients API', %q{
+  So that I can manage the Ingredients resources
+  As an API User
+  I should be able to
+} do
+
+  background do
+    Capybara.current_driver = :rack_test
+  end
+
+  context 'creating ingredients' do
+    background do
+      @recipe = Factory.create :recipe
+    end
+
+    scenario 'create recipe' do
+      page.driver.post recipe_ingredients_path(@recipe, :format => :json), :ingredient => {:name => 'Test Ingredient'}
+
+      Recipe.find_by_name('Test Recipe').should be_present
+      page.status_code.should == 201
+      page.should have_content 'Test Recipe'
+    end
+
+  end
+end
